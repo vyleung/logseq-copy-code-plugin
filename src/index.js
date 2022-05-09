18aa2e19ecfc779225cf83e4119bf0cfd666c7cf
vyleung <leung.vyz@gmail.com>
2022-05-09 10:10:19 -0700

    RELEASE: v.1.1.0
    Features
    - added: support multiple code blocks within a block
    
    Fixes
    - fixed: code copy button disappears when the block that the code block(s) is/are in exits editing mode

--- src/index.js
+++ src/index.js
@@ -21,15 +21,38 @@ const copied_icon = `<svg id="copied-icon" xmlns="http://www.w3.org/2000/svg" cl
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
   <path d="M5 12l5 5l10 -10" />
 </svg>`;
-const plugin_prefix = "#logseq-copy-code-plugin--copy-code";
 let code_blocks;
 let code_block_uuid;
+let code_block_parent;
 
 const main = async () => {
   console.log("logseq-copy-code-plugin loaded");
 
-  logseq.DB.onChanged(() => {
-    insertCopyCodeButton();
+  // doesn't detect when block is being edited (requires a new block to be added or a block's collapsed state to be toggled to run the function) - current workaround is to use mutation observer
+  // logseq.DB.onChanged(() => {
+  //   insertCopyCodeButton();
+  // });
+
+  // ref for mutation observer: https://github.com/xxchan/logseq-deadline-countdown/blob/a6cd2265b3f52d708341b47fca6e747c5c1506f8/index.js#L25-L44
+  const mutation_observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+      for (const added_node of mutation.addedNodes) {
+        if (added_node.querySelectorAll) {
+          // after exiting the code block editor, insert the copy code button
+          const code_mirror = added_node.querySelectorAll(".CodeMirror");
+          for (const code of code_mirror) {
+            insertCopyCodeButton();
+          }
+        }
+      }
+    }
+  });
+  mutation_observer.observe(parent.document.body, {
+    childList: true,
+    subtree: true
+  });
+  logseq.beforeunload(async () => {
+    mutation_observer.disconnect();
   });
 
   function insertCopyCodeButton() {
@@ -41,30 +64,39 @@ const main = async () => {
       // for plugin dev
       if (code_block.parentElement.offsetParent.offsetParent.offsetParent.classList.contains("ls-block")) {
         code_block_uuid = code_block.parentElement.offsetParent.offsetParent.offsetParent.classList[1];
+
+        // .extensions__code
+        code_block_parent = code_block.parentElement.offsetParent;
       }
       // for plugin prod
       else if (code_block.parentElement.offsetParent.offsetParent.classList.contains("ls-block")) {
         code_block_uuid = code_block.parentElement.offsetParent.offsetParent.classList[1];
+
+        // .extensions__code
+        code_block_parent = code_block.parentElement.offsetParent;
       }
       else {
         console.log("logseq-copy-code-plugin: ERROR - Cannot find code block's uuid");
       }
-
-      // copy code button (its ID is the ID of the textarea that contains the content of the code block)
+      
+      // add an ID (that's the ID of the textarea that contains the content of the code block) to div.extensions__code to differentiate multiple code blocks within one block
+      code_block_parent.id = `copy-code-${code_block.id}`;
+      
+      // inserts copy code button
       logseq.provideUI({
-        key: `copy-code-${code_block_uuid}`,
-        path: `#block-content-${code_block_uuid} > .block-body > .cp__fenced-code-block > div > .extensions__code`,
+        key: `${code_block_parent.id}`,
+        path: `#block-content-${code_block_uuid} > .block-body > .cp__fenced-code-block > div > #${code_block_parent.id}`,
         template: 
         `
-        <a class="button copy-button" id="copy-code-${code_block.id}" data-on-click="copy_code" style="display:flex; position:absolute;">
+        <a class="button copy-button" id="${code_block_parent.id}-button" data-on-click="copy_code" style="display:flex; position:absolute;">
           ${copy_icon}
         </a>
         `
       });
 
-      // container for copy code button
+      // style container for copy code button
       logseq.provideStyle(`
-        ${plugin_prefix}-${code_block_uuid} {
+        #logseq-copy-code-plugin--${code_block_parent.id} {
           position: absolute;
           z-index: 99;
           height: 0;
@@ -84,11 +116,11 @@ const main = async () => {
       window.focus();
 
       // get the content of the code block and copy it to the clipboard
-      let code_block_textarea = parent.document.getElementById(code_block_copy_button_id.split("copy-code-")[1]);
+      let code_block_textarea = parent.document.getElementById(code_block_copy_button_id.split("copy-code-")[1].split("-button")[0]);
       navigator.clipboard.writeText(code_block_textarea.value);
 
       // change the icon from copy to copied, then back to copy
-      let code_block_copy_icon = parent.document.getElementById(code_block_copy_button_id);
+      let code_block_copy_icon = parent.document.getElementById(`${code_block_copy_button_id}`);
       code_block_copy_icon.innerHTML = copied_icon;
       setDriftlessTimeout(() => {
         code_block_copy_icon.innerHTML = copy_icon;  
